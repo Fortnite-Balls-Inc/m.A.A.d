e7b9d772fa4ed552c31ca5d6b5640008ccd40375
Igor B <das.pelmene@mail.ru>
2026-09-13 18:47:00 +0300

    feat: demo in Main

--- maad/src/main/java/com/compiler/Main.java
+++ maad/src/main/java/com/compiler/Main.java
@@ -1,7 +1,23 @@
 package com.compiler;
 
+import com.compiler.lexer.Lexer;
+import com.compiler.lexer.Token;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try (BufferedReader r = new BufferedReader(new FileReader(args[0]))) {
+            Lexer l = new Lexer(r);
+            Token t = l.nextToken();
+            while (!t.isEof()) {
+                System.out.println(t);
+                t = l.nextToken();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
